e291eee94b55ada04168d35c237f5503fed6d121
Emanuelbahia <emanuelazcona@hotmail.com>
2022-05-30 18:57:56 -0300

    app nuevo

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", mainRutas);
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(3000, function () {
   console.log("funciona");
 });
